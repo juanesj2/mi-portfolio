7f0b48bdb7d54d61a4059ce1519fc10d3a6b7c74
jsalcanizTCN <jstiven@tecon.es>
2026-06-23 10:39:31 +0200

    Reduce 3D tilt effect on ProfileCard

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -85,8 +85,8 @@ const ProfileCardComponent = ({
         "--pointer-from-center": `${clamp(Math.hypot(percentY - 50, percentX - 50) / 50, 0, 1)}`,
         "--pointer-from-top": `${percentY / 100}`,
         "--pointer-from-left": `${percentX / 100}`,
-        "--rotate-x": `${round(-(centerX / 5))}deg`,
-        "--rotate-y": `${round(centerY / 4)}deg`,
+        "--rotate-x": `${round(-(centerX / 15))}deg`,
+        "--rotate-y": `${round(centerY / 15)}deg`,
       };
 
       Object.entries(properties).forEach(([property, value]) => {
